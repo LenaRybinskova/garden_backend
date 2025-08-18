9e04d64e62e5bc3bc030e339ab5b07a81ea573d9
«Elena <LenaRybinskova@gmail.com>
2025-08-18 15:02:05 +0300

    config CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Для локальной разработки
+      'https://garden-frontend-lena-3.amvera.io',
+    ],
+    credentials: true,
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   setupSwagger(app);
